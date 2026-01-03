f6b78e7a127812c248daa80dba62b300ca32a5cb
Amirmohammad <yavarnekouie@gmail.com>
2026-01-02 17:18:37 -0800

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,14 +2,14 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 def clean_data(df):
-    
+    # Handle missing values in TotalCharges
     df = df[df["TotalCharges"] != " "]
     df["TotalCharges"] = pd.to_numeric(df["TotalCharges"])
     
     if 'customerID' in df.columns:
         df.drop('customerID', axis='columns', inplace=True)
         
-    
+    # Binary encoding for categorical columns
     yes_no_columns = ['Partner','Dependents','PhoneService','MultipleLines','OnlineSecurity',
                       'OnlineBackup','DeviceProtection','TechSupport','StreamingTV','StreamingMovies','PaperlessBilling','Churn']
     for col in yes_no_columns:
@@ -17,7 +17,7 @@ def clean_data(df):
         
     df['gender'].replace({'Male': 1, 'Female': 0}, inplace=True)
     
-    # One-hot encoding
+    # One-hot encoding for multi-class categories
     df = pd.get_dummies(df, columns=['Contract','PaymentMethod','InternetService'])
     return df
 
